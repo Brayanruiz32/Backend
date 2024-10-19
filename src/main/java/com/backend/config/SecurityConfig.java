8469fe5b77cc0ad6f1c82056648651aef95a5f40
Brayanruiz32 <maverick.brm@gmail.com>
2024-10-19 18:29:26 -0500

    actualizacion

--- src/main/java/com/backend/config/SecurityConfig.java
+++ src/main/java/com/backend/config/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true); 
-        config.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        // config.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
         config.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
